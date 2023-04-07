e59e706e2acf382523ccc27e9634caa59906c02d
hcnoh <rhc0624@gmail.com>
2023-04-07 17:34:33 +0900

    modified datasets

--- data_loaders/voc2012.py
+++ data_loaders/voc2012.py
@@ -75,28 +75,28 @@ class VOC2012:
         self.val_dataset = VOC2012Dataset(self.val_imgs, self.val_lbls)
 
     def preprocess(self):
-        self.train_imgs = []
+        self.train_img_paths = []
         self.train_lbls = []
 
-        self.val_imgs = []
+        self.val_img_paths = []
         self.val_lbls = []
 
         cls_list = []
 
         with open(self.train_list_path, "r") as f:
             for line in f.readlines():
-                img, lbl = self.preprocess_one_step(line)
+                img_path, lbl = self.preprocess_one_step(line)
 
-                self.train_imgs.append(img)
+                self.train_img_paths.append(img_path)
                 self.train_lbls.append(lbl)
 
                 cls_list.extend([bbox.label for bbox in lbl])
 
         with open(self.val_list_path, "r") as f:
             for line in f.readlines():
-                img, lbl = self.preprocess_one_step(line)
+                img_path, lbl = self.preprocess_one_step(line)
 
-                self.val_imgs.append(img)
+                self.val_img_paths.append(img_path)
                 self.val_lbls.append(lbl)
 
                 cls_list.extend([bbox.label for bbox in lbl])
@@ -107,9 +107,9 @@ class VOC2012:
         with open(self.preprocessed_dataset_path, "wb") as f:
             pickle.dump(
                 (
-                    self.train_imgs,
+                    self.train_img_paths,
                     self.train_lbls,
-                    self.val_imgs,
+                    self.val_img_paths,
                     self.val_lbls,
                     self.cls_list,
                     self.cls2idx,
@@ -118,11 +118,7 @@ class VOC2012:
             )
 
     def preprocess_one_step(self, line):
-        line = line.strip()
-
-        img = Image.open(
-            os.path.join(self.imgs_path, "{}.jpg".format(line))
-        )
+        img_path = os.path.join(self.imgs_path, "{}.jpg".format(line.strip()))
 
         lbl = []
 
@@ -145,20 +141,23 @@ class VOC2012:
 
                 lbl.append(BoundingBox(x1=x1, x2=x2, y1=y1, y2=y2, label=name))
 
-        lbl = BoundingBoxesOnImage(lbl, shape=img.size)
-
-        return img, lbl
+        return img_path, lbl
 
 
 class VOC2012Dataset(Dataset):
-    def __init__(self, imgs, lbls) -> None:
+    def __init__(self, img_paths, lbls) -> None:
         super().__init__()
 
-        self.imgs = imgs
+        self.img_paths = img_paths
         self.lbls = lbls
 
     def __getitem__(self, index):
-        return index, self.imgs[index], self.lbls[index]
+        img_path = self.img_paths[index]
+
+        img = Image.open(img_path)
+        lbl = BoundingBoxesOnImage(self.lbls[index], shape=img.size)
+
+        return index, img, lbl
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.img_paths)
